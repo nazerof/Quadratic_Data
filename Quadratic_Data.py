68dbc19cd43f2b1d71718f208d4cf03d8d7e7327
Nazer <nazerof@gmail.com>
2024-05-18 13:50:08 +0200

    streamlit and requirments updates

--- Quadratic_Data.py
+++ Quadratic_Data.py
@@ -1,8 +1,4 @@
-# %%
-
-
-# %%
-#%%
+import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -10,66 +6,66 @@ import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
-
-
-
-# %%
 def create_data(n, a, b, c, noise):
     x = np.random.rand(n, 1)
     y = a * x**2 + b * x + c + np.random.randn(n, 1) * noise
     return x, y
 
-
 def save_data(x, y, filename):
-    with open(filename, 'w') as f:
+    with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['x', 'y'])
         for i in range(len(x)):
             writer.writerow([x[i][0], y[i][0]])
 
 def plot_data(x, y):
-    plt.plot(x, y, 'b.')
+    plt.figure()
+    plt.plot(x, y, 'b.', label='Data Points')
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.show()
+    plt.legend()
+    plt.grid(True)
+    return plt
 
-# %%
+def main():
+    st.title('Quadratic Data Model with Streamlit')
+    n = st.sidebar.slider('Number of points', 50, 500, 100)
+    a = st.sidebar.number_input('Coefficient a', value=3.0)
+    b = st.sidebar.number_input('Coefficient b', value=0.0)
+    c = st.sidebar.number_input('Coefficient c', value=0.25)
+    noise = st.sidebar.slider('Noise level', 0.0, 1.0, 0.1)
 
-def main(n=100, a=3, b=0, c=0.25, noise=0.1, plot=False, output='data.csv', model_output='model.pkl'):
+    output = 'data.csv'
+    model_output = 'model.pkl'
+
     x, y = create_data(n, a, b, c, noise)
     save_data(x, y, output)
-    if plot:
-        plot_data(x, y)
 
-    # Creating polynomial features
+    if st.button('Plot Data'):
+        fig = plot_data(x, y)
+        st.pyplot(fig)
+
     poly_features = PolynomialFeatures(degree=2)
     x_poly = poly_features.fit_transform(x)
-
-    # Fitting the regression model
     model = LinearRegression()
     model.fit(x_poly, y)
-
-    # Making predictions
     x_new = np.linspace(0, 1, 100).reshape(-1, 1)
     x_new_poly = poly_features.transform(x_new)
     y_pred = model.predict(x_new_poly)
 
-    # Plotting the regression line
-    plt.plot(x, y, 'b.')
-    plt.plot(x_new, y_pred, 'r-')
-    plt.title('Fitted Regression Model')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
+    if st.button('Show Regression Model'):
+        plt.figure(figsize=(8, 6))
+        plt.plot(x, y, 'b.', label='Data Points')
+        plt.plot(x_new, y_pred, 'r-', label='Regression Line')
+        plt.title('Fitted Regression Model')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend()
+        plt.grid(True)
+        st.pyplot(plt)
 
-    # Pickling the model
     with open(model_output, 'wb') as f:
         pickle.dump(model, f)
 
-
-# %%
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-
